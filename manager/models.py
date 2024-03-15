9a1c27649f1ada7c4d7cd7bf5aa66c3aaeb52ba7
Misha <mishelgrotski9@gmail.com>
2024-03-14 20:19:24 -0600

    add models and make migrations to DB with addition DB to .gitignore

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    URG = "URGENT"
+    HGH = "HIGH"
+    MDM = "MEDIUM"
+    LOW = "LOW"
+    PRIORITY_CHOICES = (
+        (URG, "Urgent"),
+        (HGH, "High"),
+        (MDM, "Medium"),
+        (LOW, "LOW")
+    )
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=20,
+        choices=PRIORITY_CHOICES,
+        default="LOW",
+    )
+    task_type = models.ForeignKey("TaskType", on_delete=models.CASCADE)
+    assignees = models.ManyToManyField()
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey("Position", on_delete=models.CASCADE)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
